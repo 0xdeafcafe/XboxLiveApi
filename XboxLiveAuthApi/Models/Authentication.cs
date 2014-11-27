37bbcb93ec5cbcf002c7d2fd03ae3e8e0f744be3
Alex Forbes-Reed (0xdeafcafe) <alexforbesreed@outlook.com>
2014-11-27 17:19:06 +0000

    Published auth api, tweaked responces.

--- XboxLiveAuthApi/Models/Authentication.cs
+++ XboxLiveAuthApi/Models/Authentication.cs
@@ -1,22 +1,31 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace XboxLiveAuthApi.Models
 {
+	[DataContract]
 	public class Authentication 
 		: Result
 	{
+		[DataMember(Name = "access_token")]
 		public string AccessToken { get; set; }
 
+		[DataMember(Name = "refresh_token")]
 		public string RefreshToken { get; set; }
 
+		[DataMember(Name = "token_type")]
 		public string TokenType { get; set; }
 
+		[DataMember(Name = "expires_in")]
 		public int ExpiresIn { get; set; }
 
+		[DataMember(Name = "expires_at")]
 		public DateTime ExpiresAt { get; set; }
 
+		[DataMember(Name = "scope")]
 		public string Scope { get; set; }
 
+		[DataMember(Name = "user_id")]
 		public long UserId { get; set; }
 	}
 }
--- XboxLiveAuthApi/Models/Response.cs
+++ XboxLiveAuthApi/Models/Response.cs
@@ -1,16 +1,24 @@
-﻿namespace XboxLiveAuthApi.Models
+﻿using System.Runtime.Serialization;
+
+namespace XboxLiveAuthApi.Models
 {
+	[DataContract]
 	public class Response
 	{
-		public Error Error {get;set;}
-
+		[DataMember(Name = "result")]
 		public Result Result { get; set; }
+
+		[DataMember(Name = "error")]
+		public Error Error {get;set;}
 	}
 
+	[DataContract]
 	public class Error
 	{
+		[DataMember(Name = "error_description")]
 		public string ErrorDescription { get; set; }
 	}
 
+	[DataContract]
 	public abstract class Result {  }
 }
